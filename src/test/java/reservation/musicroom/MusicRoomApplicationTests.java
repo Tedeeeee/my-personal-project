1806fb276a6348aabd482aeb559b0fdc6e8d84d2
Tedeeeee <gksrmf359@gmail.com>
2023-06-30 14:45:39 +0900

    개인프로젝트

---
+++ src/test/java/reservation/musicroom/MusicRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package reservation.musicroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
