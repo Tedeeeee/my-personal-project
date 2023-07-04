ee6feb3863e254a5cc45464516080e0d9ae37640
Tedeeeee <gksrmf359@gmail.com>
2023-07-04 20:01:51 +0900

    프론트 변경

--- src/main/java/reservation/musicroom/controller/room/RoomController.java
+++ src/main/java/reservation/musicroom/controller/room/RoomController.java
@@ -2,14 +2,10 @@ package reservation.musicroom.controller.room;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class RoomController {
 
-    @PostMapping("/makeRoom")
-    public String makeRoom() {
-
-        return "myProfile";
-    }
 
 }
