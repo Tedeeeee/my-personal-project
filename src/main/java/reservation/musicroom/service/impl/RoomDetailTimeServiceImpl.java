dc22444790482dec3789d4c6d1f7d14dfee8670f
Tedeeeee <gksmf35916@gmail.com>
2023-07-20 23:56:51 +0900

    feat(RoomCheck): add roomCheckByTime

--- src/main/java/reservation/musicroom/service/impl/RoomDetailTimeServiceImpl.java
+++ src/main/java/reservation/musicroom/service/impl/RoomDetailTimeServiceImpl.java
@@ -44,21 +44,16 @@ public class RoomDetailTimeServiceImpl implements RoomDetailTimeService {
                 LocalDate end = LocalDate.parse(endDay);
 
                 if (endWantDay.isAfter(start) && endWantDay.isBefore(end)) {
-                    // 예외처리
                     System.out.println("마지막 날짜가 걸림");
                 } else if (startWantDay.isAfter(start) && startWantDay.isBefore(end)) {
-                    // 예외처리
                     System.out.println("시작 날짜가 걸림");
                 } else if (startWantDay.isAfter(start) && endWantDay.isBefore(end)){
-                    // 예외처리
                     System.out.println("사용하고 있는 사이 기간임");
                 } else if (startWantDay.isBefore(start) && endWantDay.isAfter(end)) {
-                    // 예외처리
                     System.out.println("원하는 시간에 이미 예약되어있는 날짜가 있음");
                 } else {
                     String name = timeMonth.getRoomDetailName();
                     usableRoom.add(name);
-                    System.out.println(name);
                 }
             }
         }
@@ -69,6 +64,7 @@ public class RoomDetailTimeServiceImpl implements RoomDetailTimeService {
     public List<String> findRoomDetailTimeByDay(RoomDetailTimeRequestDto roomDetailTimeRequestDto) {
         List<Long> roomDetailByLocation = roomDetailMapper.findRoomDetailByLocation(roomDetailTimeRequestDto.getRoomLocation());
         String startWantDay = roomDetailTimeRequestDto.getStartDay();
+
         LocalTime startWantTime = LocalTime.parse(roomDetailTimeRequestDto.getStartTime());
         LocalTime endWantTime = LocalTime.parse(roomDetailTimeRequestDto.getEndTime());
 
@@ -78,28 +74,26 @@ public class RoomDetailTimeServiceImpl implements RoomDetailTimeService {
             List<RoomDetailTimeDto> roomDetailTimeByDay = roomDetailTimeMapper.findRoomDetailTime(value);
 
             for (RoomDetailTimeDto timeDay : roomDetailTimeByDay) {
-                String day = timeDay.getRoomDetailTimeStartDay();
-                if (timeDay.getRoomDetailTimeStartTime() == null && timeDay.getRoomDetailTimeEndTime() == null) {
-                    System.out.println("달단위로 예약되어 있는 사람");
-                }
-                LocalTime startTime = LocalTime.parse(timeDay.getRoomDetailTimeStartTime());
-                LocalTime endTime = LocalTime.parse(timeDay.getRoomDetailTimeEndTime());
+                LocalTime startTime = timeDay.getRoomDetailTimeStartTime() != null ? LocalTime.parse(timeDay.getRoomDetailTimeStartTime()) : null;
+                LocalTime endTime = timeDay.getRoomDetailTimeEndTime() != null ? LocalTime.parse(timeDay.getRoomDetailTimeEndTime()) : null;
 
-                if (!timeDay.getRoomDetailTimeStartDay().equals(startWantDay)){
+                if (startTime == null || endTime == null) {
+                    System.out.println("달단위로 예약한 사람");
+                } else if (!timeDay.getRoomDetailTimeStartDay().equals(startWantDay)) {
                     System.out.println("날짜가 달라서 괜찮아");
+                } else if (startWantTime.equals(startTime) || endWantTime.equals(endTime)) {
+                    System.out.println("시작 시간 혹은 종료 시간이 겹침");
                 } else if (endWantTime.isAfter(startTime) && endWantTime.isBefore(endTime)) {
                     System.out.println("마지막 시간이 걸림");
                 } else if (startWantTime.isAfter(startTime) && startWantTime.isBefore(endTime)) {
                     System.out.println("시작 시간이 걸림");
                 } else if (startWantTime.isBefore(startTime) && endWantTime.isAfter(endTime)) {
-                    System.out.println("이 시간은 기존에 있는 예약이랑 완전히 겹침");
+                    System.out.println("이 시간은 기존에 있는 예약이랑 겹침");
                 } else if (startWantTime.isAfter(startTime) && endWantTime.isBefore(endTime)) {
                     System.out.println("해당 시간 사이에 에약한 사람이 있음");
                 } else {
-                    System.out.println("startTime = " + startTime);
-                    System.out.println("endTime = " + endTime);
                     String name = timeDay.getRoomDetailName();
-                    System.out.println("name = " + name);
+                    usableRoom.add(name);
                 }
             }
         }
